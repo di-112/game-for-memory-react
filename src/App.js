ac275bd6924246a768ba73975dbbabead5897855
di-112 <73027626+di-112@users.noreply.github.com>
2020-12-12 15:51:52 +0300

    v 2.0

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.scss';
 import {Provider} from 'react-redux'
 import { store } from './redux/store';
-import {BrowserRouter, Redirect} from 'react-router-dom'
+import {BrowserRouter, HashRouter, Redirect} from 'react-router-dom'
 import GameWrapper from './components/gameWrapper/gameWrapper';
 
 function App() {
   return (
-    <BrowserRouter >
+    <HashRouter >
       <Provider store={store}>
         <Redirect to='/easy'></Redirect>
         <GameWrapper />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
