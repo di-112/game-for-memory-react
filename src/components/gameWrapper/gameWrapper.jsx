8167e5ef502b2e97f4a30597d288856c6e9894f7
Den <selection4test@gmail.com>
2021-09-02 16:47:03 +0300

    styles updated

--- src/components/gameWrapper/gameWrapper.jsx
+++ src/components/gameWrapper/gameWrapper.jsx
@@ -5,10 +5,11 @@ import GameGrid from '../gameGrid/gameGrid'
 import PanelStartGame from '../panelStartGame/panelStartGame'
 import Sound from '../sound/sound'
 import styles from './gameWrapper.module.scss'
+import clsx from 'clsx'
 
 const GameWrapper = (props) => {
    return (
-      <div  className={styles.wrapper + ' ' + styles[props.background]}>
+      <div className={clsx(styles.wrapper, styles[props.background])}>
          <Sound />
          <PanelStartGame />
          <GameGrid />
