304c88804851182ec223faa659a7a84618d93667
di-112 <di150499@gmail.com>
2022-01-29 01:42:51 +0300

    Изменен конфиг для webpack

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { HashRouter, Redirect } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store'
-import { COMPLEXITY } from './enums'
 import GameWrapper from './components/gameWrapper'
 import './scss/App.scss'
 
-const App = () => (
-  <HashRouter>
-    <Provider store={store}>
-      <Redirect to={`/${COMPLEXITY.EASY}`} />
-      <GameWrapper />
-    </Provider>
-  </HashRouter>
-)
+function App() {
+  return (
+    <HashRouter>
+      <Provider store={store}>
+        <GameWrapper />
+      </Provider>
+    </HashRouter>
+  )
+}
 
 export default App
