b1ce889a47e0097411bab5fb2ba84b889e337a96
Dmitry <di150499@gmail.com>
2022-01-29 17:19:42 +0300

    Добавлен app.js для heroku

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express')
+const favicon = require('express-favicon')
 
 const app = express()
+const path = require('path')
 
-app.listen(process.env.PORT || 3000)
+const port = process.env.PORT || 3000
+
+app.use(express.static('dist'))
+app.use(favicon(`${__dirname}/dist/favicon.png`))
+app.get('*', req => {
+  req.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+})
+
+app.listen(port, error => {
+  if (error) return console.log('error: ', error)
+  return console.log('Server running on post: ', port)
+})
